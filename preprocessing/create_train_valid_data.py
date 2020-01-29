68154788628193e86562dbacb31077c3c9b03185
longqing202001 <60430547+longqing202001@users.noreply.github.com>
2020-01-29 20:19:01 +0800

    Add files via upload

---
+++ preprocessing/create_train_valid_data.py
@@ -0,0 +1,132 @@
+import lib
+import argparse
+import torch
+import lib
+import numpy as np
+import os
+import datetime
+from tqdm import tqdm
+import pickle
+import pandas as pd
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--data_folder', default='/home/ryu/Desktop/project/liverpoor/data/preprocessed_data/', type=str)
+parser.add_argument('--train_data', default='retailTrainOnly.txt', type=str)
+parser.add_argument('--valid_data', default='retailValid.txt', type=str)
+parser.add_argument('--train_input_data', default='train_input.dat', type=str)
+parser.add_argument('--valid_input_data', default='valid_input.dat', type=str)
+parser.add_argument('--train_target_data', default='train_target.dat', type=str)
+parser.add_argument('--valid_target_data', default='valid_target.dat', type=str)
+parser.add_argument('--train_mask_data', default='train_mask.dat', type=str)
+parser.add_argument('--valid_mask_data', default='valid_mask.dat', type=str)
+parser.add_argument('--item_dic_data', default='item_dic.txt', type=str)
+parser.add_argument('--visitor_dic_data', default='visitor_dic.txt', type=str)
+parser.add_argument("-seed", type=int, default=22, help="Seed for random initialization") #Random seed setting
+parser.add_argument('--batch_size', default=256, type=int) #50 in first paper and 32 in second paper
+
+# Get the arguments
+args = parser.parse_args()
+args.cuda = torch.cuda.is_available()
+#use random seed defined
+np.random.seed(args.seed)
+torch.manual_seed(args.seed)
+
+if args.cuda:
+    torch.cuda.manual_seed(args.seed)
+
+use_cuda = torch.cuda.is_available()
+device = torch.device('cuda' if use_cuda else 'cpu')
+
+def save_pickle_data(dataloader,input_filepath,target_filepath,mask_filepath,batch_size,all_df_data):
+    numbers_of_N_sample=1
+    visitorid_s = []
+    input_item_s = []
+    target_item_s = []
+    mask_s = []
+    negative_item_s = []
+    df_temp = all_df_data.drop_duplicates(subset = 'item_idx', keep = 'first')
+    for ii, (visitorid, input, target, mask) in tqdm(enumerate(dataloader), total=len(dataloader.dataset.df) // dataloader.batch_size, miniters = 1000):
+            #for input, target, mask in dataloader:
+                visitorid = visitorid.to(device)
+                #print("visitorid:")
+                #print(visitorid)
+                visitorid_s=visitorid_s+visitorid.tolist()
+                input = input.to(device)
+                #print("input:") 
+                #print(input)
+                input_item_s=input_item_s+input.tolist()              
+                target = target.to(device)
+                #print("target:")  
+                #print(target)
+                target_item_s=target_item_s+target.tolist()
+                #negative_targets.append[[1]]
+                #for drop_id in visitorid:
+                #    list_ = df_temp[df_temp['visitorid'] != drop_id]['item_idx'].sample(numbers_of_N_sample).values.tolist()
+                #    negative_targets.append(list_)
+                #print("negative_targets:")
+                #print(negative_targets)     
+                #negative_item_s=negative_item_s+negative_targets
+                mask = mask.to(device)
+                mask_s.append(mask.tolist())
+
+    input_items=list(zip(visitorid_s, input_item_s))
+    print("input lenghts:"+str(len(input_items)))
+    with open(input_filepath, "wb") as f:
+        pickle.dump(input_items, f)
+
+    print("input lenghts:"+str(len(mask_s)))
+    with open(mask_filepath, "wb") as f:
+        pickle.dump(mask_s, f)
+
+    #target_items=list(zip(target_item_s,negative_item_s))
+    print("input lenghts:"+str(len(target_item_s)))
+    with open(target_filepath, "wb") as f:
+        pickle.dump(target_item_s, f)
+     
+train_filepath=os.path.join(args.data_folder, args.train_data)
+train_df = pd.read_csv(train_filepath, sep=",", usecols=["visitorid", "itemid", "timestamp"])
+print(train_df.head())
+
+valid_filepath=os.path.join(args.data_folder, args.valid_data)
+#valid_df = pd.read_csv(valid_filepath, sep=",", dtype={'visitorid': int, 'itemid': int, 'timestamp': float})
+valid_df = pd.read_csv(valid_filepath, sep=",", usecols=["visitorid", "itemid", "timestamp"])
+
+all_df_data=pd.concat([train_df, valid_df])
+
+item_ids = all_df_data['itemid'].unique()  # type is numpy.ndarray
+item2idx = pd.Series(data=np.arange(len(item_ids)),index=item_ids)
+itemmap = pd.DataFrame({'itemid': item_ids,'item_idx': item2idx[item_ids].values})
+
+visitor_ids = all_df_data['visitorid'].unique()  # type is numpy.ndarray
+visitor2idx = pd.Series(data=np.arange(len(visitor_ids)),index=visitor_ids)
+visitormap = pd.DataFrame({'visitorid': visitor_ids,'visitor_idx': visitor2idx[visitor_ids].values})
+
+item_dic_file_path=os.path.join(args.data_folder, args.item_dic_data)
+np.savetxt(item_dic_file_path, itemmap, fmt='%d')
+
+visitor_dic_file_path=os.path.join(args.data_folder, args.visitor_dic_data)
+np.savetxt(visitor_dic_file_path, visitormap, fmt='%d')
+
+train_data = lib.Dataset(os.path.join(args.data_folder, args.train_data),visitormap=visitormap, itemmap=itemmap)
+train_dataloader = lib.DataLoader(train_data, args.batch_size)
+train_dataloader.dataset.df.to_csv("./train.csv")
+
+valid_data = lib.Dataset(os.path.join(args.data_folder, args.valid_data),visitormap=visitormap, itemmap=itemmap)
+valid_dataloader = lib.DataLoader(valid_data, args.batch_size)
+valid_dataloader.dataset.df.to_csv("./valid.csv")
+
+all_df_data=pd.concat([train_dataloader.dataset.df, valid_dataloader.dataset.df])
+all_df_data.to_csv("./all.csv")
+
+# create train data
+input_filepath=os.path.join(args.data_folder, args.train_input_data)
+target_filepath=os.path.join(args.data_folder, args.train_target_data)
+mask_filepath=os.path.join(args.data_folder, args.train_mask_data)
+save_pickle_data(train_dataloader,input_filepath,target_filepath,mask_filepath,args.batch_size,all_df_data)
+
+#create valid data
+input_filepath=os.path.join(args.data_folder, args.valid_input_data)
+target_filepath=os.path.join(args.data_folder, args.valid_target_data)
+mask_filepath=os.path.join(args.data_folder, args.valid_mask_data)
+save_pickle_data(valid_dataloader,input_filepath,target_filepath,mask_filepath,args.batch_size,all_df_data)
+
---
+++ preprocessing/preprocessing_event_data.py
@@ -0,0 +1,115 @@
+import numpy as np
+import pandas as pd
+import datetime
+import pickle
+
+def removeShortSessions(data):
+    #delete sessions of length < 5
+    sessionLen = data.groupby('visitorid').size() #group by visitorid and get size of each session
+    data = data[np.in1d(data.visitorid, sessionLen[sessionLen > 2].index)]
+    return data
+
+def removeRareItems(data):
+    #delete items of length < 5
+    itemLen = data.groupby('itemid').size() #group by itemid and get size of each session
+    data = data[np.in1d(data.itemid, itemLen[itemLen > 5].index)]
+    return data
+
+preprocessed_data_path="/home/ryu/Desktop/project/liverpoor/data/raw_data/"
+dataBefore = preprocessed_data_path+'events.csv' #Path to Original Training Dataset "Clicks" File
+dataAfter = "/home/ryu/Desktop/project/liverpoor/data/preprocessed_data/" #Path to Processed Dataset Folder
+
+attribute_path=dataAfter+'item_attribute.csv'
+df=pd.read_csv(attribute_path)
+itemid=df["itemid"].values.tolist()
+
+train = pd.read_csv(dataBefore, sep=',', usecols=['visitorid','timestamp','itemid','event'])
+print(len(train))
+train= train[train['event'] == 'view']
+print(len(train))
+train=train[train.itemid.isin(itemid)]
+print(len(train))
+train.sort_values(["visitorid","timestamp"], inplace=True)
+train['match'] = (train.visitorid.eq(train.visitorid.shift()) & train.itemid.eq(train.itemid.shift()))
+train = train[~train['match']]
+
+
+# pre_visitorid=""
+# pre_itemid=""
+# i=0
+# indexes=[]
+# print(len(train))
+# for index, row in train.iterrows():
+#     if row["visitorid"]==pre_visitorid and row["itemid"]==pre_itemid:
+#         indexes.append(index)
+#         #train.drop(train.index[index])
+#     pre_visitorid=row["visitorid"]
+#     pre_itemid=row["itemid"]
+#     i+=1
+
+# with open('listfile.txt', 'w') as filehandle:
+#     for listitem in indexes:
+#         filehandle.write('%s\n' % listitem)
+
+# train=train.iloc[ indexes, : ]
+# print(len(train))
+# train.sort_values(["visitorid","timestamp"], inplace=True)
+# print(train[0:20])
+times=[]
+#dates=[]
+for i in train['timestamp']:
+    obj_datetime=datetime.datetime.fromtimestamp(i//1000.0)
+    #obj_date=obj_datetime.date()
+    times.append(obj_datetime)
+    #dates.append(obj_date)
+train['timestamp_str']=times
+#train['date']=dates
+#train.sort_values(["visitorid","itemid","date"], inplace=True)
+#train.drop_duplicates(subset=['visitorid', 'itemid', "date"], keep="last" ,inplace=True )
+print(train[0:20])
+
+train=train.drop('timestamp_str', axis=1)
+train=train.drop('match', axis=1)
+timeMin = train.timestamp.min()
+print("minimum datatime in dataset:")
+print(timeMin)
+print(datetime.datetime.fromtimestamp(timeMin//1000.0))
+
+print("maximum datatime in dataset:")
+timeMax = train.timestamp.max()
+print(timeMax)
+print(datetime.datetime.fromtimestamp(timeMax//1000.0))
+
+#dt_obj = datetime.strptime('20.12.2016 09:38:42,76','%d.%m.%Y %H:%M:%S,%f')
+#split_point = dt_obj.timestamp() * 1000
+
+split_point=datetime.datetime.strptime("2015-09-01 00:00:00.000", '%Y-%m-%d %H:%M:%S.%f').timestamp()
+#print(split_point*1000)
+#print(datetime.datetime.fromtimestamp(split_point))
+#print(datetime.datetime.fromtimestamp(split_point//1000.0))
+
+split_mask= train['timestamp']< split_point*1000
+#print(split_mask[0:20])
+
+trainTR = train[split_mask]
+#print(len(trainTR))
+
+trainVD = train[~split_mask]
+#print(len(trainVD))
+
+#Delete items in training split which are less than 5
+trainTR=removeRareItems(trainTR)
+#Delete Sessions in training split which are less than 5
+trainTR=removeShortSessions(trainTR)
+
+#Delete records in valid split where items are not in training split
+trainVD = trainVD[np.in1d(trainVD.itemid, trainTR.itemid)]
+#Delete records in valid split where visitors are not in training split
+trainVD = trainVD[np.in1d(trainVD.visitorid, trainTR.visitorid)]
+#Delete Sessions in valid split which are less than 5
+trainVD=removeShortSessions(trainVD)
+
+print('Training Set has', len(trainTR), 'Events, ', trainTR.visitorid.nunique(), 'Sessions, and', trainTR.itemid.nunique(), 'Items\n\n')
+trainTR.to_csv(dataAfter + 'retailTrainOnly.txt', sep=',', index=False)
+print('Validation Set has', len(trainVD), 'Events, ', trainVD.visitorid.nunique(), 'Sessions, and', trainVD.itemid.nunique(), 'Items\n\n')
+trainVD.to_csv(dataAfter + 'retailValid.txt', sep=',', index=False)
